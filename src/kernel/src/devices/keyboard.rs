892addea2c0d0473e4e5a4f2c1d7ef115cc99ef7
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2025-03-08 06:08:08 +0000

    feat: init executor

--- src/kernel/src/devices/keyboard.rs
+++ src/kernel/src/devices/keyboard.rs
@@ -1,6 +1,21 @@
+use core::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use conquer_once::spin::OnceCell;
+use crossbeam_queue::ArrayQueue;
+use futures_util::{
+    stream::{Stream, StreamExt},
+    task::AtomicWaker,
+};
+use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
 use x86_64::structures::idt::InterruptStackFrame;
 
 use crate::drivers::apic::{self, registers::APICRegisters};
+
+static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
+static WAKER: AtomicWaker = AtomicWaker::new();
 
 pub unsafe fn init(local_apic_ptr: *mut u32) {
     let keyboard_register =
@@ -10,36 +25,89 @@ pub unsafe fn init(local_apic_ptr: *mut u32) {
     );
 }
 
-pub extern "x86-interrupt" fn keyboard_handler(
-    _stack_frame: InterruptStackFrame,
-) {
-    use pc_keyboard::{
-        layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1,
-    };
-    use spin::{Lazy, Mutex};
-    use x86_64::instructions::port::Port;
+/// Called by the keyboard interrupt handler
+///
+/// Must not block or allocate.
+pub(crate) fn add_scancode(scancode: u8) {
+    if let Ok(queue) = SCANCODE_QUEUE.try_get() {
+        if queue.push(scancode).is_err() {
+            log::warn!("scancode queue full; dropping keyboard input");
+        } else {
+            WAKER.wake();
+        }
+    } else {
+        log::warn!("WARNING: scancode queue uninitialized");
+    }
+}
 
-    static KEYBOARD: Lazy<Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>>> =
-        Lazy::new(|| {
-            Mutex::new(Keyboard::new(
-                ScancodeSet1::new(),
-                layouts::Us104Key,
-                HandleControl::Ignore,
-            ))
-        });
+pub struct ScancodeStream {
+    _private: (),
+}
 
-    let mut keyboard = KEYBOARD.lock();
-    let mut port = Port::new(0x60);
+impl ScancodeStream {
+    pub fn new() -> Self {
+        SCANCODE_QUEUE
+            .try_init_once(|| ArrayQueue::new(100))
+            .expect("ScancodeStream::new should only be called once");
+        ScancodeStream { _private: () }
+    }
+}
 
-    let scancode: u8 = unsafe { port.read() };
-    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
-        if let Some(key) = keyboard.process_keyevent(key_event) {
-            match key {
-                DecodedKey::Unicode(character) => log::info!("{}", character),
-                DecodedKey::RawKey(key) => log::info!("{:?}", key),
+impl Stream for ScancodeStream {
+    type Item = u8;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<u8>> {
+        let queue = SCANCODE_QUEUE
+            .try_get()
+            .expect("scancode queue not initialized");
+
+        // fast path
+        if let Some(scancode) = queue.pop() {
+            return Poll::Ready(Some(scancode));
+        }
+
+        WAKER.register(cx.waker());
+        match queue.pop() {
+            Some(scancode) => {
+                WAKER.take();
+                Poll::Ready(Some(scancode))
+            }
+            None => Poll::Pending,
+        }
+    }
+}
+
+pub async fn print_keypresses() {
+    let mut scancodes = ScancodeStream::new();
+    let mut keyboard = Keyboard::new(
+        ScancodeSet1::new(),
+        layouts::Us104Key,
+        HandleControl::Ignore,
+    );
+
+    while let Some(scancode) = scancodes.next().await {
+        if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+            if let Some(key) = keyboard.process_keyevent(key_event) {
+                match key {
+                    DecodedKey::Unicode(character) => {
+                        log::info!("{}", character)
+                    }
+                    DecodedKey::RawKey(key) => log::info!("{:?}", key),
+                }
             }
         }
     }
+}
+
+pub extern "x86-interrupt" fn keyboard_handler(
+    _stack_frame: InterruptStackFrame,
+) {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    crate::devices::keyboard::add_scancode(scancode);
 
     apic::end_interrupt();
 }
