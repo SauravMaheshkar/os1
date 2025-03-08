7cdf950b0a2b4439fe5a1a2af9f9d7ce5e22f5a5
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2025-03-08 02:33:28 +0000

    feat: init IDT + GDT

---
+++ src/kernel/src/interrupts/mod.rs
@@ -0,0 +1,15 @@
+pub mod gdt;
+pub mod idt;
+
+use x86_64::structures::idt::InterruptStackFrame;
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
